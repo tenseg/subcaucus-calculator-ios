e88888ae3c5d2fa0e02ae365b17edfe596b1bfbf
efc <efc@clst.org>
2015-12-01 17:36:40 -0600

    Initial Commit

---
+++ SubCalcTests/SubCalcTests.swift
@@ -0,0 +1,36 @@
+//
+//  SubCalcTests.swift
+//  SubCalcTests
+//
+//  Created by Eric Celeste on 15.12.01.
+//  Copyright © 2015 Eric Celeste. All rights reserved.
+//
+
+import XCTest
+@testable import SubCalc
+
+class SubCalcTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
