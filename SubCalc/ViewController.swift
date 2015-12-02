61bc4e8d511a9484958289be2e0900e839986230
Alexander Celeste <alex@tenseg.net>
2015-12-02 10:19:10 -0600

    Now uses UIWebViewDelegate to ensure that only local files are loaded into the web view and all else is pushed to iOS for displaying in Mail or Safari.

--- SubCalc/ViewController.swift
+++ SubCalc/ViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import MessageUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate, MFMailComposeViewControllerDelegate {
 
+    //MARK: Vars and Lets
     @IBOutlet weak var scWebView: UIWebView!
     
+    //MARK: View Funcs
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -24,11 +27,34 @@ class ViewController: UIViewController {
         }
    }
 
+    //MARK: Memory
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    //MARK: Delegates
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+//        if request.URL!.scheme == "mailto" {
+//            if let rawParts = request.URL?.resourceSpecifier.componentsSeparatedByString("?") {
+//                if MFMailComposeViewController.canSendMail() {
+//                    let mailView = MFMailComposeViewController()
+//                    mailView.mailComposeDelegate = self
+//                    mailView.setToRecipients([rawParts[0]])
+//                    self.presentViewController(mailView, animated: true, completion: nil)
+//                }
+//            }
+//            return false
+/*        } else */ if request.URL?.scheme == "file" {
+            return true
+        } else {
+            UIApplication.sharedApplication().openURL(request.URL!)
+            return false
+        }
+    }
+    
+    func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        controller.dismissViewControllerAnimated(true, completion: nil)
+    }
 }
 
