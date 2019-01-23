4509fef9a621f6f89442b3efee72f07b060ffa78
Alexander Celeste <alex@tenseg.net>
2019-01-23 12:30:28 -0600

    web view in code, loading prams from infoplist, handling url schemes like mail

--- SubCalc/ViewController.swift
+++ SubCalc/ViewController.swift
@@ -13,28 +13,43 @@ import MessageUI
 class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, MFMailComposeViewControllerDelegate {
 
     //MARK: Vars and Lets
-    @IBOutlet weak var scWebView: WKWebView!
+    var scWebView: WKWebView!
     
     //MARK: View Funcs
     override func viewDidLoad() {
         super.viewDidLoad()
-		// attempt to load local data from user defaults
-		loadWebViewData()
-		
-		// save local data when we are no longer active
-		NotificationCenter.default.addObserver(
-			self,
-			selector: #selector(saveWebViewData),
-			name: UIApplication.willResignActiveNotification,
-			object: nil)
 		
 		// load the react app
         if let htmlPath = Bundle.main.path(forResource: "react/index", ofType: "html") {
-            let urlString = URL(fileURLWithPath: htmlPath).absoluteString + "?app=1"
-			scWebView.load(URLRequest(url: URL(string: urlString)!))
+			// set up the web view and replace our view with it
+			// there is nothing of substance in IB for this app's main GUI
+			let webConfiguration = WKWebViewConfiguration()
+			webConfiguration.websiteDataStore = WKWebsiteDataStore.default() // persistent
+			scWebView = WKWebView(frame: self.view.frame, configuration: webConfiguration)
+			scWebView.backgroundColor = UIColor(red:0.558, green:0.092, blue:0.191, alpha:1) // red
+			self.view = scWebView
+			
+			// build the url with query items passing details about the ios app
+			var queryItems = [
+				URLQueryItem(name: "app", value: nil),
+			]
+			if let version = Bundle.main.infoDictionary!["CFBundleShortVersionString"] {
+				queryItems.append(URLQueryItem(name: "version", value: String(describing: version)))
+			}
+			if let build = Bundle.main.infoDictionary!["CFBundleVersion"] {
+				queryItems.append(URLQueryItem(name: "build", value: String(describing: build)))
+			}
+			#if DEBUG
+				queryItems.append(URLQueryItem(name: "debug", value: nil))
+			#endif
+			let urlComps = NSURLComponents(string: URL(fileURLWithPath: htmlPath).absoluteString)!
+			urlComps.queryItems = queryItems
+			// load the react app
+			scWebView.load(URLRequest(url: urlComps.url!))
+			// set ourselves as the webview delegate
 			scWebView.navigationDelegate = self
 			scWebView.uiDelegate = self
-        }
+		}
 	}
 	
 	override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -48,83 +63,88 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, MFMa
 		return "<---Please don't delete the following system information--->\n\(String(describing: infoPlist!["CFBundleName"])) Version: \(String(describing: infoPlist!["CFBundleShortVersionString"])) (\(String(describing: infoPlist!["CFBundleVersion"])))\nDevice: \(device.model)\niOS Version: \(device.systemVersion))\n<------------------------------------------------>"
     }
 	
-	//MARK: Persistence
-	
-	// keys used in local storage by the web app
-	// TODO: add the correct key(s)
-	private let storageKeys = [
-		"caucuses"
-	]
-	
-	// Save data from the webview to user defaults
-	@objc func saveWebViewData() {
-		for key in storageKeys {
-			// @link https://stackoverflow.com/a/20965724
-			let jsString = "localStorage.getItem('\(key)')";
-			scWebView.evaluateJavaScript(jsString, completionHandler: { (result, error) in
-				// TODO: debug and finish once react app is actually saving data
-//				if result != nil {
-//					UserDefaults.standard.set(result, forKey: key)
-//				}
-			})
-		}
-	}
-	
-	// load data from user defaults to the web view
-	func loadWebViewData() {
-		for key in storageKeys {
-			if let localData = UserDefaults.standard.string(forKey: key) {
-				// @link https://stackoverflow.com/a/20965724
-				// TODO: debug and finish once react app is actually saving data
-//				let jsString = "localStorage.setItem('\(key)', \(localData)"
-//				scWebView.evaluateJavaScript(jsString, completionHandler: nil )
-			}
-		}
-	}
-	
 	//MARK: Actions from React App
+	
+	// share a string using ios activity view controller
 	func shareString(content: String) {
 		let activityViewController = UIActivityViewController(activityItems: [content], applicationActivities: nil)
 		present(activityViewController, animated: true, completion: nil)
 	}
 	
-	func emailTenseg() {
+	// use native Mail view to send emails
+	func email(address: String, subject: String = "SubCalc Feedback") {
 		// we want to send email feedback from native-land
 		if MFMailComposeViewController.canSendMail() {
 			let mailView = MFMailComposeViewController()
 			mailView.mailComposeDelegate = self
-			mailView.setToRecipients(["efc@tenseg.net"]) // should we really send the iOS email to subcalc@tenseg.net instead?
-			mailView.setSubject("SubCalc Feedback")
-			mailView.setMessageBody("\n\n\n \(tensegDeviceSystemProfile())", isHTML: false)
+			mailView.setToRecipients([address])
+			mailView.setSubject(subject)
+			if address == "subcalc@tenseg.net" {
+				mailView.setMessageBody("\n\n\n \(tensegDeviceSystemProfile())", isHTML: false)
+			}
 			self.present(mailView, animated: true, completion: nil)
 		} else {
-			let cantSendMailAlert = UIAlertController(title: "Cannot Send Mail", message: "You must first set up Mail before you can send feedback to Tenseg.", preferredStyle: UIAlertController.Style.alert)
-			cantSendMailAlert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+			let cantSendMailAlert = UIAlertController(title: "Cannot Send Mail", message: "You must first set up Mail before you can send feedback to Tenseg.", preferredStyle: .alert)
+			cantSendMailAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
 			self.present(cantSendMailAlert, animated: true, completion: nil)
 		}
 	}
     
     //MARK: Delegates
+	
+	// decide how to handle urls that are being loaded
 	func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: ((WKNavigationActionPolicy) -> Void)) {
 		// automatically load file URLs in this web view
 		if navigationAction.request.url?.scheme == "file" {
 			decisionHandler(.allow) // tells WKWebView to actually pick up this local file
+		// this is used for passing data between react and swift
 		} else if navigationAction.request.url?.scheme == "subcalc-extension" {
-			// this is used for passing data between react and swift
 			if let incomingString = navigationAction.request.url?.path {
-				if ( incomingString.hasPrefix("//share/" ) ) {
-					shareString(content: incomingString.deletePrefix("//share/"))
+				// used to share plaintext strings
+				if ( incomingString.hasPrefix("//share-string/" ) ) {
+					shareString(content: incomingString.deletePrefix("//share-string/"))
+				// used to send feedback emails to Tenseg
 				} else if ( incomingString == "//feedback-email" ) {
-					emailTenseg()
+					email(address: "subcalc@tenseg.net")
 				}
 			}
 			decisionHandler(.cancel) // tells WKWebView to not actually get anything
+		// emails use the in-app email view
+		} else if (navigationAction.request.url?.scheme == "mailto") {
+			email(address: navigationAction.request.url!.pathComponents[1], subject: "")
+			decisionHandler(.cancel) // tells WKWebView to not actually get anything
+		// anything else goes to Safari
 		} else {
-			UIApplication.shared.open(navigationAction.request.url!, options: [:], completionHandler: nil) // anything else goes to Safari
+			UIApplication.shared.open(navigationAction.request.url!, options: [:], completionHandler: nil)
 			decisionHandler(.cancel) // tells WKWebView to not actually get anything
 		}
 	}
-    
+	
+	// use ios alert controller for js alert panels instead of native js ones
+	// see https://medium.com/@dakeshi/tips-for-wkwebview-that-will-keep-you-from-going-troubles-c1990851385c
+	func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (() -> Void)) {
+		
+		let alertController = UIAlertController(title: "Notice", message: message, preferredStyle: .alert)
+		alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+			completionHandler()
+		}))
+		self.present(alertController, animated: true, completion: nil)
+	}
+	
+	// use ios alert controller for js confirm panels instead of native js ones
+	// see https://medium.com/@dakeshi/tips-for-wkwebview-that-will-keep-you-from-going-troubles-c1990851385c
+	func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
+		let alertController = UIAlertController(title: webView.url?.host, message: message, preferredStyle: .alert)
+		alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { action in
+			completionHandler(false)
+		}))
+		alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+			completionHandler(true)
+		}))
+		self.present(alertController, animated: true, completion: nil)
+	}
+	
+	// when finished with emails
     func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
         controller.dismiss(animated: true, completion: nil)
     }
