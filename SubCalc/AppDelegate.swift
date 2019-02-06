4479340cb24c01f2875626b3ae935ad0528e120c
Alexander Celeste <alex@tenseg.net>
2019-02-06 14:53:12 -0600

    import handled by universal links from scimport.tenseg.net, not just the subcalc://import/ scheme (see #1)

--- SubCalc/AppDelegate.swift
+++ SubCalc/AppDelegate.swift
@@ -39,6 +39,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		}
 		return false
 	}
+	
+	func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+		// if coming from universal links
+		if userActivity.activityType == NSUserActivityTypeBrowsingWeb {
+			if let urlComps = URLComponents(url: userActivity.webpageURL!, resolvingAgainstBaseURL: false) {
+				if let snapshotData = urlComps.queryValueFor("snapshot") {
+					if let viewController = self.window?.rootViewController as? ViewController {
+						viewController.importData(snapshotData)
+					}
+				}
+			}
+		}
+		return true
+	}
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- SubCalc/ViewController.swift
+++ SubCalc/ViewController.swift
@@ -110,11 +110,12 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, MFMa
 	///
 	/// - Parameters:
 	///   - data: The string data to import.
-	///   - webView: The webview to import into.
-	func importData(_ data: String, to webView: WKWebView) {
-		if var urlComps = URLComponents(url: webView.url!, resolvingAgainstBaseURL: false) {
-			urlComps.queryItems?.append(URLQueryItem(name: "snapshot", value: data))
-			webView.load(URLRequest(url: urlComps.url!))
+	func importData(_ data: String) {
+		if let webView = self.view.subviews[1] as? WKWebView {
+			if var urlComps = URLComponents(url: webView.url!, resolvingAgainstBaseURL: false) {
+				urlComps.queryItems?.append(URLQueryItem(name: "snapshot", value: data))
+				webView.load(URLRequest(url: urlComps.url!))
+			}
 		}
 	}
 	
@@ -225,9 +226,7 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, MFMa
 		//
 		// the snapshot can be the json that gets produced from the "Download code" sharing option
 		if urlComps.host == "import" {
-			if let webView = self.view.subviews[1] as? WKWebView {
-				importData(urlComps.percentEncodedPath.deletePrefix("/"), to: webView)
-			}
+			importData(urlComps.percentEncodedPath.deletePrefix("/"))
 		}
 	}
     
