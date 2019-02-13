b37f9b19d283b047c67fe88c8102e0b79fc4c7df
Alexander Celeste <alex@tenseg.net>
2019-02-13 12:11:21 -0600

    removed buildnum retrieval script as we should have it as a global script per-user now

--- retrieve-buildnum-commit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
- 
-#  retrieve-buildnum-commit.sh
-#  Usage: In project folder run `retrieve-buildnum-commit.sh n [branch]`
-# From http://tgoode.com/2014/06/05/sensible-way-increment-bundle-version-cfbundleversion-xcode/#code
- 
-branch=${2:-'master'}
-SHA1=$(git rev-list $branch | tail -n $1 | head -n 1)
-echo $SHA1
