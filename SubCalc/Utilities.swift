e615ad74e2a2bf962e97bddb325566320884160f
Alexander Celeste <alex@tenseg.net>
2019-02-28 15:25:07 -0600

    allow for react to change print job name and made clipboard paste function more universal

---
+++ SubCalc/Utilities.swift
@@ -0,0 +1,51 @@
+//
+//  Utilities.swift
+//  SubCalc
+//
+//  Created by Alexander Celeste on 2/28/19.
+//  Copyright © 2019 Tenseg.
+//
+
+import Foundation
+
+extension String {
+	/// Deletes the prefix if it exists.
+	/// See: https://www.hackingwithswift.com/example-code/strings/how-to-remove-a-prefix-from-a-string
+	///
+	/// - Parameter prefix: The string to look for and delete.
+	/// - Returns: A string withe the prefix removed if found. If not found this returns the original string.
+	func deletePrefix(_ prefix: String) -> String {
+		guard self.hasPrefix(prefix) else { return self }
+		return String(self.dropFirst(prefix.count))
+	}	
+	
+	/// Make sure the string has the given suffix.
+	///
+	/// - Parameter suffix: The desired suffix.
+	/// - Returns: The string with the suffix.
+	func ensureSuffix(_ suffix: String) -> String {
+		guard self.hasSuffix(suffix) else { return self + suffix }
+		return self
+	}
+}
+
+extension URLComponents {
+	/// Get the value for the passed query key if one exists.
+	///
+	/// The return value gets any percent encoding done for usability in a URL removed before being returned.
+	///
+	/// - Parameter key: The key to look for.
+	/// - Returns: The string value in the query for the key that was passed or nil if not found.
+	func queryValueFor(_ key: String) -> String? {
+		if let queryItems = self.queryItems {
+			for item in queryItems {
+				if item.name == key {
+					if let value = item.value?.removingPercentEncoding {
+						return value
+					}
+				}
+			}
+		}
+		return nil
+	}
+}
--- SubCalc/ViewController.swift
+++ SubCalc/ViewController.swift
@@ -262,14 +262,12 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, MFMa
 	func retrieveClipboardContents() {
 		if let pastedString = UIPasteboard.general.string {
 			if let urlComps = URLComponents(string: pastedString) {
-				if urlComps.host == "scimport.tenseg.net" {
-					importQuery(urlComps.queryItems)
-					return // skip the alert about invalid contents, since clipboard contents were valid
-				}
+				importQuery(urlComps.queryItems)
+			} else {
+				let alertController = UIAlertController(title: "Clipboard Not a URL", message: "Your clipboard isn't a URL. Only URLs work to be pasted into SubCalc", preferredStyle: .alert)
+				alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+				self.present(alertController, animated: true, completion: nil)
 			}
-			let alertController = UIAlertController(title: "Invalid Clipboard Contents", message: "Your clipboard isn't a SubCalc import URL.", preferredStyle: .alert)
-			alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-			self.present(alertController, animated: true, completion: nil)
 		} else {
 			let alertController = UIAlertController(title: "Clipboard Empty", message: "There is nothing on your clipboard to paste.", preferredStyle: .alert)
 			alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
@@ -281,11 +279,14 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, MFMa
 	/// Displays the print UI to allow the user to print the currently-displayed conents of the web view
 	/// needs @objc because it is called, in part, from a keyboard shortcut
 	/// see https://nshipster.com/uiprintinteractioncontroller/
-	@objc func printWebContent() {
+	///
+	/// - Parameters:
+	///   - jobName: The name of the print job, may show up as PDF filename or on some printer displays.
+	@objc func printWebContent(_ jobName: String = "SubCalc") {
 		if let webView = self.view.subviews[1] as? WKWebView {
 			// set up print info
 			let printInfo = UIPrintInfo(dictionary: nil)
-			printInfo.jobName = "SubCalc"
+			printInfo.jobName = jobName
 			printInfo.duplex = .longEdge
 			printInfo.outputType = .grayscale
 			
@@ -348,7 +349,8 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, MFMa
 		//
 		// you can also trigger printing with command-p
 		if urlComps.host == "print" {
-			printWebContent()
+			let jobName = urlComps.queryValueFor("name") ?? "SubCalc"
+			printWebContent(jobName)
 		}
 	}
     
@@ -356,22 +358,24 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, MFMa
 	
 	// decide how to handle urls that are being loaded
 	func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: ((WKNavigationActionPolicy) -> Void)) {
-		if let urlComps = URLComponents(string: navigationAction.request.url?.absoluteString ?? "") {
-			// automatically load file URLs in this web view
-			if urlComps.scheme == "file" {
-				decisionHandler(.allow) // tells WKWebView to actually pick up this local file
-			// this scheme is used to pass data between react and swift
-			} else if urlComps.scheme == "subcalc" {
-				handleSubcalcURLComponents(urlComps)
-				decisionHandler(.cancel) // tells WKWebView to not actually get anything
-			// open mailto urls in mailview instead of Safari
-			} else if urlComps.scheme == "mailto" {
-				// example url: "mailto:email@Mailto.co.uk?subject=Subject Using Mailto.co.uk&body=Email test"
-				sendEmailTo(urlComps.path, withSubject: urlComps.queryValueFor("subject"), andBody: urlComps.queryValueFor("body"))
-				decisionHandler(.cancel) // tells WKWebView to not actually get anything
-			} else {
-				UIApplication.shared.open(navigationAction.request.url!, options: [:], completionHandler: nil)
-				decisionHandler(.cancel) // tells WKWebView to not actually get anything
+		if let urlString = navigationAction.request.url?.absoluteString {
+			if let urlComps = URLComponents(string: urlString) {
+				// automatically load file URLs in this web view
+				if urlComps.scheme == "file" {
+					decisionHandler(.allow) // tells WKWebView to actually pick up this local file
+				// this scheme is used to pass data between react and swift
+				} else if urlComps.scheme == "subcalc" {
+					handleSubcalcURLComponents(urlComps)
+					decisionHandler(.cancel) // tells WKWebView to not actually get anything
+				// open mailto urls in mailview instead of Safari
+				} else if urlComps.scheme == "mailto" {
+					// example url: "mailto:email@Mailto.co.uk?subject=Subject Using Mailto.co.uk&body=Email test"
+					sendEmailTo(urlComps.path, withSubject: urlComps.queryValueFor("subject"), andBody: urlComps.queryValueFor("body"))
+					decisionHandler(.cancel) // tells WKWebView to not actually get anything
+				} else {
+					UIApplication.shared.open(navigationAction.request.url!, options: [:], completionHandler: nil)
+					decisionHandler(.cancel) // tells WKWebView to not actually get anything
+				}
 			}
 		}
 	}
@@ -442,59 +446,5 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, MFMa
 	
 	deinit {
 		NotificationCenter.default.removeObserver(self)
-	}
-}
-
-extension String {
-	/// Deletes the prefix if it exists.
-	/// See: https://www.hackingwithswift.com/example-code/strings/how-to-remove-a-prefix-from-a-string
-	///
-	/// - Parameter prefix: The string to look for and delete.
-	/// - Returns: A string withe the prefix removed if found. If not found this returns the original string.
-	func deletePrefix(_ prefix: String) -> String {
-		guard self.hasPrefix(prefix) else { return self }
-		return String(self.dropFirst(prefix.count))
-	}
-	
-	
-	/// Deletes the suffix if it exists.
-	/// See: https://www.hackingwithswift.com/example-code/strings/how-to-remove-a-prefix-from-a-string
-	///
-	/// - Parameter suffix: The string to look for and delete.
-	/// - Returns: A string withe the suffix removed if found. If not found this returns the original string.
-	func deleteSuffix(_ suffix: String) -> String {
-		guard self.hasSuffix(suffix) else { return self }
-		return String(self.dropLast(suffix.count))
-	}
-	
-	
-	/// Make sure the string has the given suffix.
-	///
-	/// - Parameter suffix: The desired suffix.
-	/// - Returns: The string with the suffix.
-	func ensureSuffix(_ suffix: String) -> String {
-		guard self.hasSuffix(suffix) else { return self + suffix }
-		return self
-	}
-}
-
-extension URLComponents {
-	/// Get the value for the passed query key if one exists.
-	///
-	/// The return value gets any percent encoding done for usability in a URL removed before being returned.
-	///
-	/// - Parameter key: The key to look for.
-	/// - Returns: The string value in the query for the key that was passed or nil if not found.
-	func queryValueFor(_ key: String) -> String? {
-		if let queryItems = self.queryItems {
-			for item in queryItems {
-				if item.name == key {
-					if let value = item.value?.removingPercentEncoding {
-						return value
-					}
-				}
-			}
-		}
-		return nil
 	}
 }
